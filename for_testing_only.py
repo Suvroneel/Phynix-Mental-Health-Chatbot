d6c2fb9081ceef79fbe832795b22ca5b21705065
Suvroneel Nathak <61592625+Suvroneel@users.noreply.github.com>
2025-11-07 11:00:59 +0530

    Delete for_testing_only.py

--- for_testing_only.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-pages = [
-    st.Page("pages/1_Chat.py", title="Chat", icon=":material/chat_bubble:"),
-    st.Page("pages/2_Home.py", title="Home", icon=":material/home:"),
-]
-
-pg = st.navigation(pages)
-pg.run()
